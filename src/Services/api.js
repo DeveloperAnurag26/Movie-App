950f2a23b251dc3cb793a685fcdb0ad7e12a97bc
Anurag Pandey <anuragpandey537755@gmail.com>
2025-09-21 07:54:17 +0530

    change api key and add to it on .env file

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,18 +1,14 @@
-const API_KEY = "e0b1fb66e1662602027c6aeea4ecaa99";
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3";
 
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
 
-export const getPopularMovies = async ()=>{
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await response.json()
-    return data.results
-}
-
-export const searchMovies = async (query) =>{
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-        query
-    )}`
-);
-const data = await response.json();
-return data.results;
-}
+export const searchMovies = async (query) => {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+  const data = await response.json();
+  return data.results;
+};
